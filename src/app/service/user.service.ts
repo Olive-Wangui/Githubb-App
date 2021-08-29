2caf0e5417597c0e83e98a4903c8b6e2c94b44cd
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-08-29 17:06:38 +0300

    Change from Http to HttpHeaders.

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class UserService {
   private clientId ='cad08ebafb9bb94cc353';
   private clientsecret ='21ae41656595ef9e5b84cf04c53238d8c4aaabf7';
 
-  constructor(private http:Http) {
+  constructor(private http:HttpHeaders) {
     console.log("User service is now running!");
     this.username = "Olive";
    }
